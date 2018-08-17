09e4df3824df0a5415ddeb86a6640bff183e2cda
Seungje Jun <ninanung@naver.com>
2018-08-17 11:36:39 +0900

    delete view folder and app option

--- server/app.js
+++ server/app.js
@@ -9,10 +9,6 @@ var pop3 = require('./routes/pop3.js');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,12 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/imap', imap);
 app.use('/pop3', pop3);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
