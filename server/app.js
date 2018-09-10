fffb3848d6e6f48ed09e77218785a270073faa0c
Seungje Jun <ninanung@naver.com>
2018-09-10 17:36:53 +0900

    constant and more route

--- server/app.js
+++ server/app.js
@@ -5,12 +5,25 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+var app = express();
+
 var imap = require('./routes/imap');
 var signup = require('./routes/account/signup');
 var signin = require('./routes/account/signin');
 var get_user = require('./routes/account/get_user')
+var get_all_email_by_address = require('./routes/get_email/get_all_email_by_address');
+var get_email_by_address = require('./routes/get_email/get_email_by_address')
+var get_email_unseen = require('./routes/get_email/get_email_unseen');
+var get_sent_email_by_address = require('./routes/get_email/get_sent_email_by_address')
 
-var app = express();
+app.use('/imap', imap);
+app.use(post + '/signup', signup);
+app.use(get + '/signin', signin);
+app.use(get + '/user', get_user);
+app.use(get + '/email/:address', get_email_by_address);
+app.use(get + '/email/all/:address', get_all_email_by_address);
+app.use(get + '/email/unseen', get_email_unseen);
+app.use(get + '/email/sent/:address', get_sent_email_by_address);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,10 +34,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'html');
 
-app.use('/imap', imap);
-app.use('/api/signup', signup);
-app.use('/api/signin', signin);
-app.user('/api/get/user', get_user);
+const get = '/api/get';
+const post = '/api/post';
 
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ server/routes/account/update_user.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+var User = require('../../models/users');
+
+router.post('/', function(req, res, next) {
+    var body = req.body;
+    var info = {
+        error: '',
+        words: '',
+    }
+    User.findOne({ id = body.id }, function(err, user) {
+        if(err) {
+            info.error = 'true';
+            info.words = err;
+            console.log(err);
+            return res.send(info);
+        }
+        if(!user) {
+            info.error = 'true';
+            info.words = 'There\'s no account exist.';
+            return res.send(info);
+        }
+        user.id = body.id,
+        user.password = body.password,
+        user.imap_id = body.imap_id,
+        user.imap_password = body.imap_password,
+        user.imap_host = body.imap_host,
+        user.imap_port = body.imap_port,
+        user.imap_tls = body.imap_tls,
+        user.smtp_id = body.smtp_id,
+        user.smtp_password = body.smtp_password,
+        user.smtp_host = body.smtp_host,
+        user.smtp_port = body.smtp_port,
+        user.smtp_secure = body.smtp_secure
+        user.save(function(err){
+            if(err) {
+                info.error = "true";
+                info.words = "데이터베이스 오류가 발생했습니다. 다시 시도해 주세요";
+                return res.send(info);
+            }
+        })
+        return res.send(info);
+    });
+});
+
+module.exports = router;
