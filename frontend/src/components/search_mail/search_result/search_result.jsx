02273274343e4273458b05cb9ff5f1018b00ef97
Seungje Jun <ninanung@naver.com>
2018-11-12 16:56:11 +0900

    search result

--- frontend/src/components/search_mail/search_result/search_result.jsx
+++ frontend/src/components/search_mail/search_result/search_result.jsx
@@ -18,12 +18,70 @@ class SearchResult extends React.Component {
         }
     }
 
-    componentWillMount() {
-
+    componentWillReceiveProps(nextProps) {
+        const mails = this.props.mails.slice();
+        const text = nextProps.searchText
+        const searchedMails = [];
+        mails.map((mail, index) => {
+            const cc = mail.cc.slice();
+            const to = mail.to.slice();
+            if(mail.subject.includes(text) || mail.from.includes(text)) {
+                searchedMails.push(mail);
+            }
+            else if(mail.text && mail.text.includes(text)) {
+                searchedMails.push(mail);
+            } else if(mail.html && mail.html.includes(html)) {
+                searchedMails.push(mail);
+            } else if(mail.name && mail.name.includes(text)) {
+                searchedMails.push(mail);
+            } else if(cc.length) {
+                for(let i = 0; i < cc.length; i++) {
+                    cc[i].includes(text);
+                    searchedMails.push(mail);
+                    break;
+                }
+            } else if(to.length) {
+                for(let i = 0; i < to.length; i++) {
+                    to[i].includes(text)
+                    searchedMails.push(mail);
+                    break;
+                }
+            }
+        })
+        searchedMails = this.insertDate(searchedMails);
+        this.setState({
+            searchedMails: searchedMails.slice(),
+        })
     }
 
-    componentWillReceiveProps(nextProps) {
+    insertDate = (sortedArray) => {
+        if(sortedArray.length === 0) return sortedArray;
+        const copy = sortedArray;
+        for(let i = 0; i < sortedArray.length; i++) {
+            if(i === 0) {
+                const info = {
+                    date: this.parseToString(sortedArray[i].date),
+                    isDateBar: true,
+                }
+                copy.splice(i, 0, info);
+            } else if(!sortedArray[i]) return copy;
+            else {
+                const lastDate = this.parseToString(sortedArray[i - 1].date)
+                const thisDate = this.parseToString(sortedArray[i].date)
+                if(lastDate !== thisDate) {
+                    const info = {
+                        date: this.parseToString(sortedArray[i].date),
+                        isDateBar: true,
+                    }
+                    copy.splice(i, 0, info);
+                }
+            }
+        }
+        return copy;
+    }
 
+    parseToString = (date) => {
+        return new Date(date).toString().split(" ").splice(0, 4).splice(0, 4).toString().replace(/,/g, " ");
     }
 
     render() {
